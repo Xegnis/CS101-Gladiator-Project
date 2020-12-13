4b0c98ca2ad0126f7da568a451d1eda2bd92f396
nankuta <nicholas.ankuta@gmail.com>
2020-12-13 13:45:36 -0500

    Update GBSimulator.java

--- classes/GBSimulator.java
+++ classes/GBSimulator.java
@@ -107,5 +107,64 @@ public class GBSimulator
 			}
 		}
 	}
-
+	
+	public static boolean WriteToTabDelimitedFile(String[] data, String FileName)
+	{
+		try
+		{
+			FileWriter fileWritter = new FileWriter(FileName);
+			BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+			// loop through all your data and print it to the file
+			for (int i=0;i< data.length;i++)
+					bufferWritter.write(data[i]+"\t");
+			bufferWritter.write("\n");
+			bufferWritter.close();
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error Printing Tab Delimited File");
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean appendToTabDelimitedFile (String[] data, String FileName)
+	{
+		try
+		{
+			File file =new File(FileName);
+			if(!file.exists()) //if file doesn’t exists, return false
+				return false;
+			FileWriter fileWritter = new FileWriter(file.getName(),true);
+			BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+			for (int i=0;i< data.length;i++)
+				bufferWritter.write(data[i]+"\t");
+			bufferWritter.write("\n");
+			bufferWritter.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return true;
+	}
+		
+	public static String [] readFromTabDelimitedFile (String FileName)
+	{
+		List<String> lines = new ArrayList<String>();
+		try
+		{
+			FileReader fileReader = new FileReader(FileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null)
+				lines.add(line);
+			bufferedReader.close();
+		}
+		catch (IOException e)
+		{
+		e.printStackTrace();
+		}
+		return lines.toArray(new String[lines.size()]);
+	}	
 }
